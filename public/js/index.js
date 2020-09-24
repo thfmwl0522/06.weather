b9567411cc8e9d5d82a47f0513eb37a1a4a5cc29
thfmwl0522 <thfmwl0522@naver.com>
2020-09-24 14:35:39 +0900

    9/24 수업

---
+++ public/js/index.js
@@ -0,0 +1,7 @@
+/********************* 전역설정 ****************************/
+var appid = '5f7a63efe450492bfa3c6505476ef424';
+var dailyURL = 'http://api.openweathermap.org/data/2.5/weather';
+var weeklyURL = 'http://api.openweathermap.org/data/2.5/forecast';
+var sendData = { units:'metric', lang : 'kr', appid : appid }
+
+console.log(sendData.appid);
